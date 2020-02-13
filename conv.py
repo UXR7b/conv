d76f4df27cfc545f87adc3cf231a0423eb4ab284
aFro <57800606+UXR7b@users.noreply.github.com>
2020-02-13 22:09:46 +1000

    Add files via upload

--- conv.py
+++ conv.py
@@ -7,6 +7,7 @@ print(token)
 sess=vk.Session(access_token=token)
 api=vk.API(sess,v='5.92',lang='ru')
 id = input("ID:")
+double = int(input(">200? 1/0"))
 memberslist = str(api.messages.getConversationMembers(peer_id=2000000000+int(id)))
 members1=memberslist.rsplit("'profiles'")
 members2=members1[0]
@@ -20,6 +21,8 @@ for n in range(1,memberslen):
 	
 
 message = ''
+message1 = ''
+message2 = ''
 ss=0
 memberslen2=len(members)
 for s in range(0,int(memberslen2/2)):
@@ -27,9 +30,26 @@ for s in range(0,int(memberslen2/2)):
 	if members5 < 0:
 		members.remove(str(members5))
 		
-		
+double = 0
 memberslen2=len(members)
+if memberslen2 > 200:
+    double = 1
+timer = 0
+stade = 1
 for n in range(ss,memberslen2):
-	message += '[id'+members[n]+'| ] '
+    if double == 0:
+        message += '[id'+members[n]+'| ] '
+    else:
+        if timer > 250:
+            api.messages.send(random_id=int(time.time()),peer_id=2000000000+int(id),message=message)
+            message = ''
+        timer += 1
+        message += '[id'+members[n]+'| ] '
+        
+    
+        
+        
+if double == 0:
+    api.messages.send(random_id=int(time.time()),peer_id=2000000000+int(id),message=message)
 
-api.messages.send(random_id=int(time.time()),peer_id=2000000000+int(id),message=message)
+    
